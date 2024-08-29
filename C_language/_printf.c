d28f89cea8c9a29299818730c6cf8b9035c960eb
Aden1ke <sarahogunlalu@.com>
2024-08-29 13:40:19 +0100

    added specifiers(c,s,d,i)

--- C_language/_printf.c
+++ C_language/_printf.c
@@ -2,6 +2,7 @@
 
 int _printf(const char *format, ...) {
 	int i, j;
+	int printed_chars = 0;
 	char character;
 	char *str;
 	int num;
@@ -24,12 +25,14 @@ int _printf(const char *format, ...) {
 				case 'c':
 					character = va_arg(args, int);
 					add_to_buffer(character);
+					printed_chars++;
 					break;
 				case 's':
 					str = va_arg(args, char*);
 					for (j = 0; str[j] != '\0'; j++)
 					{
 						add_to_buffer(str[j]);
+						printed_chars++;
 					}
 					break;
 				case 'd':
@@ -38,26 +41,30 @@ int _printf(const char *format, ...) {
 					sprintf(num_str, "%d", num);
 					for (j = 0; num_str[j] != '\0'; j++) {
 						add_to_buffer(num_str[j]);
+						printed_chars++;
 					}
 					break;
 				case '%':
 					add_to_buffer('%');
+					printed_chars++;
 					break;
 				default:
 					add_to_buffer('%');
 					add_to_buffer(format[i]);
+					printed_chars += 2;
 					break;
 					
 			}
 		}
 		else {
 			add_to_buffer(format[i]);
+			printed_chars++;
 		}
 	}
 	// Clean up the va_list
 	va_end(args);
 
-    return 0;
+    return printed_chars;
 }
 
 
